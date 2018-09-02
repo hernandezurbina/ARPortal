cf261183f57b6021bdac661d3522d6b325b94a0f
Victor Hernandez-Urbina <hernandezurbina@vico.local>
2018-09-02 17:16:00 +0100

    Plane detection with label functionality added

--- ARPortal/ViewController.swift
+++ ARPortal/ViewController.swift
@@ -9,18 +9,25 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     
+    @IBOutlet weak var planeDetectedLabel: UILabel!
     let configuration = ARWorldTrackingConfiguration()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.delegate = self
+        
         self.sceneView.session.run(configuration)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +35,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return}
+        
+        DispatchQueue.main.async {
+            self.planeDetectedLabel.isHidden = false
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.planeDetectedLabel.isHidden = true
+        }
+    }
 
 }
 
